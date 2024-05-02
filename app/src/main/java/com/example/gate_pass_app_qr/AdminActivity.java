1b5f8610615a23bbd9132650c38dc574349b16c8
Ravi <ravikantkispotta2@gmail.com>
2024-05-02 14:32:06 +0530

    Token genertation is Done now just need to replace the EditText with the QR system

--- app/src/main/java/com/example/gate_pass_app_qr/AdminActivity.java
+++ app/src/main/java/com/example/gate_pass_app_qr/AdminActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +15,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import java.util.Objects;
 
@@ -22,9 +35,10 @@ public class AdminActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     private FirebaseAuth authProfile;
     private TextView message;
+    private EditText editTextRedeemToken;
+    private FirebaseFirestore db;
     private static final String TAG = "LoginActivity";
 
-    private String adminType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,11 +54,12 @@ public class AdminActivity extends AppCompatActivity {
 
         message = findViewById(R.id.textViewAdmin);
         message.setText("Welcome Gate-Keeper");
+        editTextRedeemToken = findViewById(R.id.editText_redeem_token);
 
         authProfile = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         Button logout = findViewById(R.id.button_logout);
-
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -55,5 +70,49 @@ public class AdminActivity extends AppCompatActivity {
             }
         });
 
+        Button buttonRedeemToken = findViewById(R.id.button_Redeem_token);
+        buttonRedeemToken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String token = editTextRedeemToken.getText().toString();
+                redeemToken(token);
+            }
+        });
+    }
+
+    private void redeemToken(String token) {
+
+        // Check if the token exists and is not redeemed
+        db.collection("tokens").whereEqualTo("token", token)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            DocumentSnapshot tokenDoc = queryDocumentSnapshots.getDocuments().get(0);
+                            Boolean redeemed = tokenDoc.getBoolean("redeemed");
+                            if (redeemed != null && !redeemed) {
+                                // Mark the token as redeemed
+                                tokenDoc.getReference().update("redeemed", true,
+                                        "redeemedBy", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                                // Handle token redemption logic here
+                                Toast.makeText(AdminActivity.this, "Token Redeemed Successfully.", Toast.LENGTH_SHORT).show();
+                            } else {
+                                // Token is already redeemed
+                                Toast.makeText(AdminActivity.this, "Token is Already Redeemed.", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            // Token does not exist
+                            Toast.makeText(AdminActivity.this, "Token does Not Exist.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle error
+                        Toast.makeText(AdminActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/gate_pass_app_qr/TeacherActivity.java
+++ app/src/main/java/com/example/gate_pass_app_qr/TeacherActivity.java
@@ -2,8 +2,11 @@ package com.example.gate_pass_app_qr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,13 +17,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 public class TeacherActivity extends AppCompatActivity {
     private FirebaseAuth authProfile;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseFirestore db;
     private TextView message;
+    private EditText editTextTokenReason, editTextTokenStudentName, editTextTokenStudentId;
+    private String tokenReason, studentName, studentId;
     private static final String TAG = "TeacherActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,10 +55,16 @@ public class TeacherActivity extends AppCompatActivity {
         message = findViewById(R.id.textViewAdmin);
         message.setText("Welcome Faculty Member");
 
+        editTextTokenReason = findViewById(R.id.editText_token_reason);
+        editTextTokenStudentName = findViewById(R.id.editText_token_student_name);
+        editTextTokenStudentId = findViewById(R.id.editText_token_student_id);
+
+
         authProfile = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         Button logout = findViewById(R.id.button_logout);
-
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,5 +74,69 @@ public class TeacherActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        // Generate token button click listener
+        Button generateToken = findViewById(R.id.button_generate_token);
+        generateToken.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tokenReason = editTextTokenReason.getText().toString();
+                studentName = editTextTokenStudentName.getText().toString();
+                studentId = editTextTokenStudentId.getText().toString();
+
+                if(TextUtils.isEmpty(tokenReason)){
+                    editTextTokenReason.setError("Reason is Required.");
+                    editTextTokenReason.requestFocus();
+                }else if (TextUtils.isEmpty(studentName)){
+                    editTextTokenStudentName.setError("This Field is Required. Enter Student's name.");
+                    editTextTokenStudentName.requestFocus();
+                }else if (TextUtils.isEmpty(studentId)){
+                    editTextTokenStudentId.setError("This Field is Required. Enter Student's Enrollment Number.");
+                    editTextTokenStudentId.requestFocus();
+                }else{
+                    // create a progress bar.
+
+                    //generate and save token to firebase Database.
+                    generateAndSaveToken();
+                }
+            }
+        });
     }
+
+    private void generateAndSaveToken() {
+        // Generate a unique token
+        String token = generateUniqueToken();
+
+        //String teacherId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        // Save the token to Firebase
+        Map<String, Object> tokenData = new HashMap<>();
+        tokenData.put("token", token);
+        tokenData.put("studentId", studentId);
+        tokenData.put("studentName", studentName);
+        tokenData.put("createdAt", System.currentTimeMillis());
+        tokenData.put("redeemed", false);
+
+        db.collection("tokens").add(tokenData)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        // Token saved successfully
+                        Toast.makeText(TeacherActivity.this, "Token saved/ Generated.", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle error
+                        Toast.makeText(TeacherActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.e(TAG, e.getMessage());
+                    }
+                });
+    }
+    private String generateUniqueToken() {
+        // Implement your token generation logic here
+        return UUID.randomUUID().toString();
+    }
+
 }
